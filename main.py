5ac2067d874a31b35317c5cc9193cbf674649532
sheronjay <192441684+sheronjay@users.noreply.github.com>
2025-12-28 00:59:19 +0530

    add comments

--- main.py
+++ main.py
@@ -17,31 +17,46 @@ def get_input() -> tuple[Path, str]:
 
     if not video_path.exists():
         print(f"Error: {video_path} does not exist.")
-        sys.exit(1)sinhala_audio
+        sys.exit(1)
 
     return video_path, model_name
 
 def main():
     # Get user input for video path and model(audio to english txt) name
     video_path, model_name = get_input()
+    print(f"Video Path: {video_path}")
+    print(f"Model Name: {model_name}")
 
     # Convert video to audio
+    print("Converting video to audio...")
     audio_path = convert_to_audio(video_path, video_path.with_suffix('.mp3'))
+    print(f"Audio Path: {audio_path}")
 
     # Generate speech-to-text transcription
+    print("Transcribing audio...")
     transcribe_path = transcribe_audio(audio_path, model_name)
+    print(f"Transcribe Path: {transcribe_path}")
 
     # Translate transcription to Sinhala
+    print("Translating transcription to Sinhala...")
     translated_path = translate_file(transcribe_path)
+    print(f"Translated Path: {translated_path}")
 
     # Romanize Sinhala text
+    print("Romanizing Sinhala text...")
     romanized_path = romanize(translated_path)
+    print(f"Romanized Path: {romanized_path}")
 
     # Generate sinhala audio using tts model
+    print("Generating Sinhala audio...")
     sinhala_wav = sinhala_audio(romanized_path)
+    print(f"Sinhala Audio Path: {sinhala_wav}")
 
     # Join the original video with the new Sinhala audio
-    join_video_audio(video_path, sinhala_wav)
+    print("Joining original video with Sinhala audio...")
+    sinhala_video_path = join_video_audio(video_path, sinhala_wav)
+    print(f"Sinhala Video Path: {sinhala_video_path}")
+
 
 
 if __name__ == "__main__":
